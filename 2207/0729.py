0efb70ba8b10f04af67ab53726ae0f8b32ce33ec
손민혁 <mh.blosson@gmail.com>
2022-07-30 19:04:52 +0900

    0730

--- 2207/0729.py
+++ 2207/0729.py
@@ -1,11 +1,21 @@
 
-
 T = int(input())
 
-
 for i in range(T):
-    N = int(input())
-    game_money = N * 100
-    max_game = int(game_money / 2)
+    N = int(input()) # 솔직히 이건 의미가 없지 않나? N을 근거로 N_ppl에서 리스트를 받아올 때 제약을 줄 수는 없을까?
+    N_ppl = list(map(int, input().split())) # map 함수  다시 공부하자..
+    # print(N_ppl) 왜 이건 그냥 주소가 뜨고
+    # print(list(N_ppl)) 이건 이건 리스트 형태로 뜰까
 
-    print(f'#{i+1} {max_game}')
+    count = 0
+    while True:
+        N_ppl.sort(reverse=True) # 정렬해서 가장 큰 2개 보여주기
+        if N_ppl[1] != 0: # 2번째로 큰 숫자가 0이면(게임을 못하게 됨)
+            N_ppl[0] -= 1
+            N_ppl[1] -= 1
+            count += 1
+        else:
+            break
+    print(f'#{i+1} {count}')
+
+    
--- 2207/test.py
+++ 2207/test.py
@@ -1,3 +1,46 @@
-list = ['-', '-', '-']
+'''list = ['-', '-', '-']
 
-print(*list)
+print(*list)'''
+
+T = int(input()) #테스트케이스 개수
+test_list = [] #일괄출력하고자 한다 -> 알려주신거 
+
+for i in range(1, T+1): #1부터 테스트케이스까지를 
+    N = int(input())
+    a,b,c,d,e = 0,0,0,0,0
+    test_list_2 = []
+    
+    if N%2 ==0: #2로 나눠진다면
+        while N%2 ==0: #2로 나누는 걸 계속 하고
+            N = N/2 #나눌때마다 한번씩 더 2로 나누고 
+            a = a+1 #그럴때마다 test_list_2리스트의 a에 1씩 추가한다.
+    test_list_2.append(a)
+
+    if N%3 ==0:
+        while N%3 ==0:
+            N = N/3
+            b = b+1
+    test_list_2.append(b)
+
+    if N%5 ==0:
+        while N%5 ==0:
+            N = N/5
+            c = c+1
+    test_list_2.append(c)
+
+    if N%7 ==0:
+        while N%7 ==0:
+            N = N/7
+            d = d+1
+    test_list_2.append(d)
+
+    if N%11 ==0:
+        while N%11 ==0:
+            N = N/11
+            e = e+1
+    test_list_2.append(e)
+    test_list.append(test_list_2) #이걸 추가했더니 거의 정답에 가까워졌다.
+	
+    for j in range(len(test_list)): 
+        print(f'#{j+1}', end=' ')
+        print(*test_list[j])
