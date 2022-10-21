aaeb5e2282b2968b0e0fbb8c2cdf3cfd34e5b2f2
손민혁 <mh.blosson@gmail.com>
2022-10-21 17:52:53 +0900

    alarm done

--- BOJ_starting_from_the_bottom/if/2884.py
+++ BOJ_starting_from_the_bottom/if/2884.py
@@ -2,10 +2,12 @@ hour, minute = map(int, input().split())
 
 new_minute = minute - 45
 
-
-
 if new_minute < 0:
     hour -= 1
-    
+    new_minute += 60
+    if hour == -1:
+        hour = 23
+
+print(hour, new_minute)
 
 
