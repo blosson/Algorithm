3e7d136ec92e6ad3f9c260d3749df6ee40e993a7
손민혁 <mh.blosson@gmail.com>
2022-11-02 08:27:49 +0900

    up

--- BOJ_starting_from_the_bottom/for_while/15552.py
+++ BOJ_starting_from_the_bottom/for_while/15552.py
@@ -1,5 +1,11 @@
 
-T = int(input())
+# T = int(input())
+# for _ in range(T):
+#     A, B = map(int, input().split())
+#     print(A+B)
+
+import sys
+T = int(sys.stdin.readline())
 for _ in range(T):
-    A, B = map(int, input().split())
+    A, B = map(int, sys.stdin.readline().split())
     print(A+B)
