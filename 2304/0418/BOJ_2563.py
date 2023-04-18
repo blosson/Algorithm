126a46967ebe29040aa50a6bbf3521c0203cd802
Minhyeok Son <mh.blosson@gmail.com>
2023-04-18 13:14:41 +0900

    230418

---
+++ 2304/0418/BOJ_2563.py
@@ -0,0 +1,21 @@
+# BOJ_2563 S1
+
+num = int(input())
+paper = [[0] * 100 for _ in range(100)]
+
+# 10*10의 색종이의 x, y 좌표값을 기반으로 순회해서 리스트의 idx값을 0->1로 바꿔줌
+for _ in range(num):
+    width, height = map(int, input().split())
+    for i in range(width, width+10):
+        for j in range(height, height+10):
+            paper[i][j] = 1
+
+# 전체 paper 순회해서 1인 부분이 있으면 넓이에 더해줌
+ans = 0                      
+for i in range(100):
+    for j in range(100):
+        if paper[i][j] == 1:
+            ans += 1
+
+print(ans)
+            
