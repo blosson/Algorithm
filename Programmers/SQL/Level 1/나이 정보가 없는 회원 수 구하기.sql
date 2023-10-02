c5b90af68359234a14f9ff7abad86ce4fe462e19
손민혁 <mh.blosson@gmail.com>
2023-10-02 19:22:24 +0900

    programmers sql basics

---
+++ Programmers/SQL/Level 1/나이 정보가 없는 회원 수 구하기.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131528
+# AS를 사용하면 해당 컬럼으로 출력됨
+SELECT COUNT(*) AS USERS FROM USER_INFO WHERE AGE IS NULL
+
+# 다른 풀이
+# SELECT SUM(AGE IS NULL) AS USERS FROM USER_INFO
