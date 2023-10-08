2f9dd18355dba8b8140c85b3d717885be22f6192
Minhyeok Son <mh.blosson@gmail.com>
2023-10-08 10:51:36 +0900

    SQL substr practice

---
+++ Programmers/SQL/Level 2/카테고리 별 상품 개수 구하기.sql
@@ -0,0 +1,5 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131529
+-- SUBSTRING(컬럼, 시작IDX, 시작IDX 기준 몇개나) (SQL은 IDX 1부터 시작함)
+SELECT SUBSTR(PRODUCT_CODE, 1, 2) AS CATEGORY, COUNT(*) AS PRODUCTS FROM PRODUCT
+GROUP BY CATEGORY
+ORDER BY CATEGORY ASC
