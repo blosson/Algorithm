40484f7b3e06d553d6e94c38f1c0bf5459d0f915
Minhyeok Son <mh.blosson@gmail.com>
2023-10-24 16:01:03 +0900

    mac git setting

--- Programmers/SQL/Level 1/12세 이하인 여자 환자 목록 출력하기.sql
+++ Programmers/SQL/Level 1/12세 이하인 여자 환자 목록 출력하기.sql
@@ -1,5 +1,5 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/132201
--- IFNULL
+-- IFNULL (null 조건 시)
 SELECT PT_NAME, PT_NO, GEND_CD, AGE, IFNULL(TLNO, 'NONE') AS TLNO FROM PATIENT
 WHERE GEND_CD = 'W' AND AGE <= 12 
 ORDER BY AGE DESC, PT_NAME ASC
