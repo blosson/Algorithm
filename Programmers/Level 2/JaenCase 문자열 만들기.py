a5947c96d1d16856a36b126887be76e72e23baad
Minhyeok Son <mh.blosson@gmail.com>
2023-11-01 12:09:38 +0900

    str capitalize

--- Programmers/Level 2/JaenCase 문자열 만들기.py
+++ Programmers/Level 2/JaenCase 문자열 만들기.py
@@ -1,3 +1,24 @@
+# <IDEA>
+# join 함수, string의 upper, lower, capitalize 특성 이용
+
+def solution(s):
+    s = s.split(" ")
+    for i in range(len(s)):
+        s[i] = s[i][:1].upper() + s[i][1:].lower()
+    return ' '.join(s)
+
+
+def solution(s):
+    answer = ''
+    s=s.split(' ')
+    for i in range(len(s)):
+        # capitalize 내장함수를 사용하면 첫 문자가 알파벳일 경우 대문자로 만들고
+        # 두번째 문자부터는 자동으로 소문자로 만든다
+        # 첫 문자가 알파벳이 아니면 그대로 리턴한다
+        s[i]=s[i].capitalize()
+    answer=' '.join(s)
+    return answer
+
 
 def solution(s):
     answer = ''
